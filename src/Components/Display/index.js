4083381d5ed046721b5045528d7ff4ac824d32af
Arindam Chowdhury <arindamchy00@gmail.com>
2023-07-03 22:21:10 +0530

    Added Display component to show data in Table

--- src/Components/Display/index.js
+++ src/Components/Display/index.js
@@ -1,3 +1,28 @@
-export const Display = () => {
-  return
+import styles from "./display.module.css"
+
+export const Display = ({ data }) => {
+	return (
+		<>
+			{data.length > 0 && (
+				<div className={styles.tableContainer}>
+					<table className={styles.table}>
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Age</th>
+							</tr>
+						</thead>
+						<tbody>
+							{data.map(item => (
+								<tr key={item.id}>
+									<td>{item.name}</td>
+									<td>{item.age}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			)}
+		</>
+	)
 }
