821f04e50b593dce9664117b289c1f9f2ca6d0e3
Arindam Chowdhury <arindamchy00@gmail.com>
2023-07-03 23:24:19 +0530

    1. Fixed DB name typo in DBManager 2. Provided correct path for DB in DBManager for Build mode 3. Added configs for DB Build in package.json

--- public/Database/dbManager.js
+++ public/Database/dbManager.js
@@ -1,5 +1,10 @@
 const Database = require("better-sqlite3")
-const dbPath = "./demo_table.db"
+const path = require("path")
+
+const dbPath =
+	process.env.NODE_ENV === "development"
+		? "./demo_table.db"
+		: path.join(process.resourcesPath, "./demo_table.db")
 
 const db = new Database(dbPath)
 db.pragma("journal_mode = WAL")
