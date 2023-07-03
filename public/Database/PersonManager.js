25fc6121ae5eb18e6a44446458c2c19f80e1e3b1
Arindam Chowdhury <arindamchy00@gmail.com>
2023-07-03 22:20:25 +0530

    Fixed insert query syntax error

--- public/Database/PersonManager.js
+++ public/Database/PersonManager.js
@@ -2,37 +2,37 @@ const dbmgr = require("./dbManager")
 const db = dbmgr.db
 
 const readAllPerson = () => {
-  try {
-    const query = `SELECT * FROM person`
-    const readQuery = db.prepare(query)
-    const rowList = readQuery.all()
-    return rowList
-  } catch (err) {
-    console.error(err)
-    throw err
-  }
+	try {
+		const query = `SELECT * FROM person`
+		const readQuery = db.prepare(query)
+		const rowList = readQuery.all()
+		return rowList
+	} catch (err) {
+		console.error(err)
+		throw err
+	}
 }
 
 const insertPerson = (name, age) => {
-  try {
-    const insertQuery = db.prepare(
-      `INSERT INTO person (name, age) VALUES (${name} , ${age})`
-    )
+	try {
+		const insertQuery = db.prepare(
+			`INSERT INTO person (name, age) VALUES ('${name}' , ${age})`
+		)
 
-    const transaction = db.transaction(() => {
-      const info = insertQuery.run()
-      console.log(
-        `Inserted ${info.changes} rows with last ID ${info.lastInsertRowid} into person`
-      )
-    })
-    transaction()
-  } catch (err) {
-    console.error(err)
-    throw err
-  }
+		const transaction = db.transaction(() => {
+			const info = insertQuery.run()
+			console.log(
+				`Inserted ${info.changes} rows with last ID ${info.lastInsertRowid} into person`
+			)
+		})
+		transaction()
+	} catch (err) {
+		console.error(err)
+		throw err
+	}
 }
 
 module.exports = {
-  readAllPerson,
-  insertPerson,
+	readAllPerson,
+	insertPerson,
 }
