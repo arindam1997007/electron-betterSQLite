37afb58b5ae170f392d19be43e4d54d3b236fe0d
Arindam Chowdhury <arindamchy00@gmail.com>
2023-07-03 22:21:46 +0530

    1. Made design changes to App.js 2. Save data to DB on submit in InputPerson 3. Fetching data from DB in App.js 4. Rendering InputPerson and Display in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
+import { useEffect, useState } from "react"
 import "./App.css"
 import { Display } from "./Components/Display"
 import { InputPerson } from "./Components/InputPerson"
+import { useCallback } from "react"
 
 function App() {
-  return (
-    <>
-      <div className="background">
-        <div className="App">
-          <Display />
-          <InputPerson />
-        </div>
-        <span>The background is provided by loading.io </span>
-      </div>
-    </>
-  )
+	const [data, setData] = useState([])
+
+	const fetchData = useCallback(() => {
+		const data = window.sqlite.personDB?.readAllPerson()
+		setData(data)
+	}, [])
+
+	useEffect(() => {
+		fetchData()
+	}, [fetchData])
+
+	return (
+		<>
+			<div className='background'>
+				<div className='App'>
+					<InputPerson fetchData={fetchData} />
+					<Display data={data} />
+				</div>
+				<span>The background is provided by loading.io </span>
+			</div>
+		</>
+	)
 }
 
 export default App
--- src/Components/InputPerson/index.js
+++ src/Components/InputPerson/index.js
@@ -1,18 +1,32 @@
 import styles from "./inputPerson.module.css"
 
-export const InputPerson = () => {
+export const InputPerson = ({ fetchData }) => {
+	const onSubmit = e => {
+		e.preventDefault()
+		const name = e.target.name?.value
+		const age = e.target.age?.value
+		if (!name || !age) return
+
+		window.sqlite.personDB?.insertPerson(name, parseInt(age))
+		setTimeout(() => {
+			// TODO: handle it asynchronously
+			fetchData(), 200
+		})
+		e.target.reset()
+	}
+
 	return (
 		<>
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={onSubmit}>
 				<fieldset>
 					<legend>Enter Details</legend>
 					<div className={styles.form_field}>
 						<label htmlFor='name'>Name</label>
-						<input type='text' id='name' name='details' />
+						<input type='text' id='name' name='name' />
 					</div>
 					<div className={styles.form_field}>
 						<label htmlFor='age'>Age</label>
-						<input type='number' id='age' name='details' />
+						<input type='number' id='age' name='age' />
 					</div>
 					<div className={styles.form_submit}>
 						<input type='submit' value='ADD' />
